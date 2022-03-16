c5f6e3adc2f73cf61e351e051dc79c9f7db073d7
Kasib <kasibzxc@gmail.com>
2022-03-15 23:05:12 -0400

    add interactive text

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+
+// needs to
+
+int main () {
+
+    // C1*V1 = C2*V2
+    int source;
+
+    cout << "Source concentration (1 if undiluted): ";
+    cin >> source;
+
+    // create function to solve for vol needed (vol 1)
+    int neededvol;
+
+    int desired;
+    cout << "Desired concentration: ";
+    cin >> desired;
+
+    // create function to solve for nearest volume
+    int totalvol;
+    cout << "Total volume: ";
+    cin >> totalvol;
+
+
+}
