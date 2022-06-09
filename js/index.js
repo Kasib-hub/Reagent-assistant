cc1983ceebfb76083b4fbe624b6b6381b0f5bcad
Kasib <kasibzxc@gmail.com>
2022-06-09 01:22:59 -0400

    fractions implemented successfully

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 
+// find a way to change input to two values separated by a '/' symbol
+const buttonText = document.querySelector('.fraction-btn');
+const sourceC1 = document.querySelector('#source-concentration');
+const finalC2 = document.querySelector('#solution-concentration');
+const finalV2 = document.querySelector('#solution-volume');
+const division = document.querySelector('.nested');
+
+buttonText.addEventListener('click', () => {
+    const initialButtonText = 'Enter as Fraction';
+
+    if (buttonText.innerHTML.includes(initialButtonText)) {
+        buttonText.innerHTML = 'Enter as Decimal or Whole Number';
+        finalC2.style.display = 'none';
+        division.style.display = 'inline-block';
+
+    } else {
+        buttonText.innerHTML = initialButtonText;
+        division.style.display = 'none';
+        finalC2.style.display = 'inline-block';
+    }
+});
+
 // numbers must be greater than 0. Floats allowed
 function checkInput(x, y, z) {
     if (!Number.isNaN(x, y, z) &&
@@ -12,37 +34,23 @@ function checkInput(x, y, z) {
 
 // can't get numbers from users until they click submit
 function show() {
-    let C1 = document.getElementById('source-concentration').value;
-    let C2 = document.getElementById('solution-concentration').value;
-    let V2 = document.getElementById('solution-volume').value;
+    let C1 = sourceC1.value;
+    let C2 = finalC2.value;
+    let V2 = finalV2.value;
+
+    if (buttonText.innerHTML.includes('Enter as Decimal')) {
+        C2 = document.querySelector('.numerator').value / document.querySelector('.denominator').value;
+    }
 
     // checkInput works as intended
     if (checkInput(C1, C2, V2)) {
+        console.log('values are valid');
         document.getElementById('source-volume').placeholder = C2 * V2 / C1;
-    }
-
-    console.log('The values are: ' + C1 + ' ' + C2 + ' ' + V2);
-}
-
-
-// find a way to change input to two values separated by a '/' symbol
-const buttonText = document.querySelector('.fraction-btn');
-
-buttonText.addEventListener('click', () => {
-    const initialButtonText = 'Enter as Fraction';
-
-    if (buttonText.innerHTML.includes(initialButtonText)) {
-        buttonText.innerHTML = 'Enter as Decimal or Whole Number';
-        document.querySelector('#solution-concentration').style.display = 'none';
-        document.querySelector('.nested').style.display = 'inline-block';
-
     } else {
-        buttonText.innerHTML = initialButtonText;
-        document.querySelector('.nested').style.display = 'none';
-        document.querySelector('#solution-concentration').style.display = 'inline-block';
+        alert('Enter valid Digits');
     }
-});
 
+}
 
 
 
