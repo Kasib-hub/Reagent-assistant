0e6310c34e4cb6988389ef4065133116186d72e1
Kasib <kasibabdullah8@gmail.com>
2023-01-05 22:02:18 -0500

    cleaned up comments on api call

--- assistant/dilution/views.py
+++ assistant/dilution/views.py
@@ -6,9 +6,10 @@ import requests
 # home route is making an api call to wikipedia to pull an antigen system
 # How would I get the search term from my front end? Perhaps wait until react
 def home(request):
-    message = 'The app is launched as base'
     url = 'https://en.wikipedia.org/w/api.php'
     search = 'Duffy_antigen_system'
+
+    # params to get the first 2 sentences with a wiki article
     params = f'?action=query&prop=extracts&titles={search}&format=json&exsentences=2&explaintext=1&formatversion=2'
     response = requests.get(url + params)
 
@@ -16,4 +17,4 @@ def home(request):
     data = response.json()
     search_text = data['query']['pages'][0]
 
-    return render(request, 'dilution/base.html', {'message': message, 'data': search_text})
+    return render(request, 'dilution/base.html', {'data': search_text})
